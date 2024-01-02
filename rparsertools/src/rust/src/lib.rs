8ef70c0d74eba30dc9a85313b2a50fe3b8901ada
Devin Pastoor <devin.pastoor@gmail.com>
2024-01-02 11:58:12 -0500

    feat: first cut at r wrapper

--- rparsertools/src/rust/src/lib.rs
+++ rparsertools/src/rust/src/lib.rs
@@ -1,4 +1,7 @@
 use extendr_api::prelude::*;
+mod depedencies;
+mod constraints;
+use crate::depedencies::{packages_list};
 
 /// Return string `"Hello world!"` to R.
 /// @export
@@ -6,11 +9,23 @@ use extendr_api::prelude::*;
 fn hello_world() -> &'static str {
     "Hello world!"
 }
+// in the future consider https://josiahparry.com/posts/2023-11-24-dfusionrdr/#handling-arrow-rs-from-r
 
+#[extendr]
+// @export
+fn parse_package_dependencies(package: &str, deps: &str) -> std::result::Result<List, String> {
+   let parsed_deps = packages_list(deps);
+   if parsed_deps.is_err() {
+      return Err("Error parsing dependencies".to_string());
+   }
+    let parsed_deps = parsed_deps.unwrap();
+   let dep_pkgs: Vec<String> = parsed_deps.iter().map(|dep| dep.name.clone() ).collect();
+   Ok(list!(name = package, deps = dep_pkgs ))
+}
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod rparsertools;
-    fn hello_world;
+    fn parse_package_dependencies;
 }
